d66bfe953e4a1ebcb7a348f9b4b769c0b2adb49b
stevenacz <scoailazaa@gmail.com>
2020-11-04 23:56:34 -0500

    Problema 08

--- 08contacts/contacts.rb
+++ 08contacts/contacts.rb
@@ -19,108 +19,128 @@ contacts = {
 # Modify the file below this line
 # --------------------------------------------------
 
+# Srting
+def srting
+  print("Name: ")
+  gets.to_s.strip
+end
+
+# Details
+def details
+  print("Country: ")
+  country = gets.to_s.strip
+  print("Email: ")
+  email = gets.to_s.strip
+  print("Phone: ")
+  phone = gets.to_s.strip
+  [country, email, phone]
+end
+
+# Display
+def display(contacts)
+  puts("--------------------------------------------------")
+  puts("All your contacts:")
+  contacts.each do |k, _v|
+    puts("- #{k}")
+  end
+end
+
+# Add
+def add(name, contacts)
+  if contacts.key?(name)
+    puts("Contact already exists")
+  else
+    new_details = details
+    contacts[name] = {
+      "country" => new_details[0],
+      "email" => new_details[1],
+      "phone" => new_details[2]
+    }
+  end
+end
+
+# Show
+def show(name, contacts)
+  puts("-" * 50)
+  if contacts.key?(name)
+    contacts[name].each do |k, v|
+      puts("- #{k}: #{v}")
+    end
+  else
+    puts("Contact doesn't exist")
+  end
+end
+
+# Update
+def update(name, contacts)
+  if contacts.key?(name)
+    new_details = details
+    contacts[name] = {
+      "country" => new_details[0],
+      "email" => new_details[1],
+      "phone" => new_details[2]
+    }
+  else
+    puts("Contact doesn't exist")
+  end
+end
+
+# Delete
+def delete(name, contacts)
+  puts("-" * 50)
+  if contacts.key?(name)
+    contacts.delete(name)
+    puts("#{name} has been deleted")
+  else
+    puts("Contact doesn't exist")
+  end
+end
+
+# Welcome
+def welcome
+  puts("#{'-' * 50}\nWelcome to Contacts\n#{'-' * 50}")
+  puts("What would you like to do next?")
+  puts("-- Type 'display' to display all contacts")
+  puts("-- Type 'add' to add a contact")
+  puts("-- Type 'show' to show the details of a contact")
+  puts("-- Type 'update' to update a contact")
+  puts("-- Type 'delete' to delete a contact")
+  puts("-- Type 'exit' to exit the program")
+  print("action: ")
+end
+
+# Next step
+def next_step
+  puts("--------------------------------------------------")
+  puts("What would you like to do next?")
+  print("action: ")
+end
+
 n_loop = 0
 loop do
-  if n_loop == 0
-    puts("--------------------------------------------------")
-    puts("Welcome to Contacts")
-    puts("--------------------------------------------------")
-    puts("What would you like to do next?")
-    puts("-- Type 'display' to display all contacts")
-    puts("-- Type 'add' to add a contact")
-    puts("-- Type 'show' to show the details of a contact")
-    puts("-- Type 'update' to update a contact")
-    puts("-- Type 'delete' to delete a contact")
-    puts("-- Type 'exit' to exit the program")
-    print("action: ")
-    action = gets.to_s.strip
-  else
-    puts("--------------------------------------------------")
-    puts("What would you like to do next?")
-    print("action: ")
-    action = gets.to_s.strip
-  end
-  
+  n_loop.zero? ? welcome : next_step
+  action = gets.to_s.strip
   case action
-    when "display"
-      puts("--------------------------------------------------")
-      puts("All your contacts:")
-      contacts.each do |k, _v|
-        puts("- #{k}")
-      end
-    when "add"
-      print("Name: ")
-      name = gets.to_s.strip
-
-      if contacts.key?(name)
-        puts("--------------------------------------------------")
-        puts("Contact already exists")
-      else
-        puts("--------------------------------------------------")
-        print("Country: ")
-        country = gets.to_s.strip
-        print("Email: ")
-        email = gets.to_s.strip
-        print("Phone: ")
-        phone = gets.to_s.strip
-
-        contacts.merge!({name => {
-          "country" => country,
-          "email" => email,
-          "phone" => phone
-        }})
-      end
-    when "show"
-      print("Contact name: ")
-      name = gets.to_s.strip
-      
-      if contacts.key?(name)
-        puts("--------------------------------------------------")
-        contacts[name].each do |k, v|
-          puts("- #{k}: #{v}")
-        end
-      else
-        puts("--------------------------------------------------")
-        puts("Contact doesn't exist")
-      end
-    when "update"
-      print("Contact name: ")
-      name = gets.to_s.strip
-
-      if contacts.key?(name)
-        puts("--------------------------------------------------")
-        print("Country: ")
-        country = gets.to_s.strip
-        print("Email: ")
-        email = gets.to_s.strip
-        print("Phone: ")
-        phone = gets.to_s.strip
-
-        contacts[name] = {
-          "country" => country,
-          "email" => email,
-          "phone" => phone
-        }
-      else
-        puts("--------------------------------------------------")
-        puts("Contact doesn't exist")
-      end
-    when "delete"
-      print("Contact name: ")
-      name = gets.to_s.strip
-
-      if contacts.key?(name)
-        contacts.delete(name)
-        puts("--------------------------------------------------")
-        puts("Diego has been deleted")
-      else
-        puts("--------------------------------------------------")
-        puts("Contact doesn't exist")
-      end
-    when "exit"
-      puts("Bye!")
-      break
+  when "display"
+    display(contacts)
+  when "add"
+    name = srting
+    puts("-" * 50)
+    add(name, contacts)
+  when "show"
+    name = srting
+    show(name, contacts)
+  when "update"
+    name = srting
+    puts("-" * 50)
+    update(name, contacts)
+  when "delete"
+    name = srting
+    delete(name, contacts)
+  when "exit"
+    puts("Bye! #{break}")
+  else
+    puts("Invalid action")
   end
-
   n_loop += 1
 end
